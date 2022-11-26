6d7f69cbf3c375c12d7bec10ab430a0e9b4599e4
duranbe <benoit.durand.mail@gmail.com>
2022-11-26 18:46:02 +0100

    Testing autocomplete + add HTTP 400

--- backend/main.py
+++ backend/main.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI, Request
+from fastapi import FastAPI, Request, HTTPException
 from fastapi.middleware.cors import CORSMiddleware
 import motor.motor_asyncio
 
@@ -22,20 +22,33 @@ client = motor.motor_asyncio.AsyncIOMotorClient(
 
 db = client["restaurants"]
 
-
 @app.get("/search")
 async def main(request: Request):
     kw = request.query_params.get('keyword', None)
+    
+    if kw is None or len(kw)<1:
+        raise HTTPException(status_code=400, detail="Wrong keyword search")
 
     results = (await db["restaurants-reviews"].aggregate([
         {
-            '$search': {
+            "$search": {
                 'index': 'reviews',
-                'text': {
-                    'query': kw,
-                    'path': {
-                        'wildcard': '*'
-                    }
+                "compound": {
+                    "should": [{
+                        "text": {
+                            "query": kw,
+                            'path': {
+                                'wildcard': '*'
+                            }
+                        }
+                    },
+                        {
+                        "text": {
+                            "query": kw,
+                            "path": "Type",
+                            "score": {"boost": {"value": 3}}
+                        }
+                    }]
                 }
             }
         },
@@ -49,8 +62,45 @@ async def main(request: Request):
                          "Type": 1,
                          "Location": 1,
                          "Price_Range": 1,
-                         "Street Address": 1}
+                         "Street Address": 1,
+                         "score": {"$meta": "searchScore"}
+                         }
         }
     ]).to_list(length=None))
+
+    return results
+
+
+
+# No meant to be used for now
+@app.get("/autocomplete")
+async def autocomplete_results(request: Request):
+
+    kw = request.query_params.get('keyword', None)
+
+    results = (await db["restaurants-reviews"].aggregate([
+        {
+            '$search': {
+                'index': 'reviews',
+                'autocomplete': {
+                    'path': 'Type',
+                    'query': kw,
+                },
+                "highlight": {
+                    "path": "Type"
+                }
+            }
+        }, {
+            '$limit': 3
+        }, {
+            '$project': {
+                "id": {'$toString': "$_id"},
+                "_id": 0,
+                'Type': 1,
+                "highlights": { "$meta": "searchHighlights" }
+            }
+        }
+    ]).to_list(length=None))
+
 
     return results
--- frontend/src/App.js
+++ frontend/src/App.js
@@ -79,7 +79,7 @@ function App() {
             </svg>
 
           </span>
-          <input onKeyPress={handleKeyPress} className="font-clash-light placeholder:italic placeholder:text-slate-400 block bg-white w-full border border-slate-300 rounded-md py-2 pl-9 pr-3 shadow-sm focus:outline-none focus:border-sky-500 focus:ring-sky-500 focus:ring-1 sm:text-sm font-light" placeholder="Search for anything..." type="text" name="search" />
+          <input onKeyPress={handleKeyPress} className="font-clash-regular placeholder:italic placeholder:text-slate-400 block bg-white w-full border border-slate-300 rounded-md py-2 pl-9 pr-3 shadow-sm focus:outline-none focus:border-sky-500 focus:ring-sky-500 focus:ring-1 sm:text-sm font-light" placeholder="Search for anything..." type="text" name="search" />
         </label>
       </div>
 
