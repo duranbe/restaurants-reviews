f561731ec6e577d2214f1dd880a79303f7536990
duranbe <benoit.durand.mail@gmail.com>
2022-11-28 10:31:44 +0100

    Minor frontend update

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -7,7 +7,7 @@ import { useState } from 'react';
 function RestaurantCard({ restaurantData }) {
 
   return (
-    <div className="my-5">
+    <div className="my-3 p-3 hover:cursor-pointer hover:border-black hover:border-2 rounded-md">
 
       <div className="underline underline-offset-[3px]  font-clash-medium hover:italic cursor-pointer">
         {restaurantData.Name}
@@ -60,14 +60,22 @@ function App() {
 
     setData({ data: [] })
 
-    if (e.key === "Enter") {
+   
+    if (e.key === "Enter" || e.type==="click") {
 
       setLoadingState(true);
       setLandingPage(false);
+      
+      let searchValue;
+      if(e.type === "click"){
+        searchValue = document.getElementById("search-bar").value;
+      }else{
+        searchValue = e.target.value;
+      }
 
       axios.get('http://localhost:8000/search', {
         params: {
-          keyword: e.target.value,
+          keyword: searchValue,
           vegan: isVegan,
         }
       })
@@ -92,15 +100,15 @@ function App() {
   return (
     <div className="mx-5">
       <NavBar />
-      <div className="mt-4 mb-2 mx-10 ">
+      <div className="mt-4 mb-2 mx-10">
         <label className="relative block font-light text-xl5">
 
-          <input onKeyPress={handleKeyPress} className="h-12 font-clash-regular placeholder:italic
+          <input autocomplete="off"  id="search-bar" onKeyPress={handleKeyPress} className="h-12 font-clash-regular placeholder:italic
           
            placeholder:text-slate-400 block bg-white w-full border border-slate-300 rounded-md
             p-2 shadow-sm focus:outline-none focus:border-sky-500 
             focus:ring-sky-500 focus:ring-1 sm:text-sm font-light" placeholder="Search for anything..." type="text" name="search" />
-          <span className="absolute inset-y-0 right-0 flex items-center pr-3">
+          <span className="absolute inset-y-0 right-0 flex items-center pr-3 cursor-pointer" onClick={handleKeyPress}>
             <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={2} stroke="currentColor" className="w-5 h-5">
               <path strokeLinecap="round" strokeLinejoin="round" d="M21 21l-5.197-5.197m0 0A7.5 7.5 0 105.196 5.196a7.5 7.5 0 0010.607 10.607z" />
             </svg>
@@ -108,12 +116,19 @@ function App() {
           </span>
         </label>
         <div className="flex items-center mt-2 ml-3">
-          <div className="flex">
+          <div className="flex mx-1">
             <input type="checkbox" id="choose-me" className="peer hidden" onChange={toggleVegan} />
             <label htmlFor="choose-me" className="select-none cursor-pointer rounded-lg border-1
             py-1 px-2 font-clash-medium transition-colors duration-100 ease-in-out
-            bg-slate-500 text-gray-50 text-m border-2 hover:italic
+            bg-slate-500 text-gray-50 text-m border-2 
            peer-checked:bg-gray-200 peer-checked:text-gray-900 peer-checked:border-slate-400">Vegan</label>
+          </div>
+          <div className="flex mx-1">
+            <input type="checkbox" id="choose-nyc" className="peer hidden" onChange={toggleVegan} />
+            <label htmlFor="choose-nyc" className="select-none cursor-pointer rounded-lg border-1
+            py-1 px-2 font-clash-medium transition-colors duration-100 ease-in-out
+            bg-slate-500 text-gray-50 text-m border-2 
+           peer-checked:bg-gray-200 peer-checked:text-gray-900 peer-checked:border-slate-400">New York City</label>
           </div>
         </div>
 
