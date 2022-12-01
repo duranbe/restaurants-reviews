c11819ba8d6a54c5501d57876d5f99d2fd4b9bc3
duranbe <benoit.durand.mail@gmail.com>
2022-12-01 22:56:39 +0100

    Remove bar on navbar, looks better now

--- frontend/src/components/NavBar.jsx
+++ frontend/src/components/NavBar.jsx
@@ -2,7 +2,8 @@
 export default function NavBar() {
 
     return (
-      <nav className="font-clash-bold border-gray-400 relative w-full flex flex-wrap items-center justify-between py-2 bg-orange-50">
+      <nav className="font-clash-bold border-none relative w-full flex flex-wrap 
+      items-center justify-between pt-2 bg-orange-50">
         <div className="container-fluid w-full flex flex-wrap items-center justify-between px-3 ">
           <a className="flex items-center text-2xl" href="/">
             CookReview
