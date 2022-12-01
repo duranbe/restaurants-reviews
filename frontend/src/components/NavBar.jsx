acd1638b76c6d16701f7290b72cb3963351fee92
duranbe <benoit.durand.mail@gmail.com>
2022-12-01 16:04:48 +0100

    update frontend

--- frontend/src/components/NavBar.jsx
+++ frontend/src/components/NavBar.jsx
@@ -2,7 +2,7 @@
 export default function NavBar() {
 
     return (
-      <nav className="font-clash-bold border-gray-300 relative w-full flex flex-wrap items-center justify-between py-2 bg-gray-100">
+      <nav className="font-clash-bold border-gray-400 relative w-full flex flex-wrap items-center justify-between py-2 bg-orange-50">
         <div className="container-fluid w-full flex flex-wrap items-center justify-between px-3 ">
           <a className="flex items-center text-2xl" href="/">
             CookReview
--- frontend/src/components/RestaurantPage.jsx
+++ frontend/src/components/RestaurantPage.jsx
@@ -4,6 +4,8 @@ import { useState, useEffect } from 'react';
 import axios from 'axios';
 
 
+const svgPath = process.env.PUBLIC_URL + '/svg/';
+
 export default function RestaurantPage() {
     const { id } = useParams();
 
@@ -35,44 +37,54 @@ export default function RestaurantPage() {
         return (<div>Loading...</div>)
     } else {
         return (
-            <div className='container my-8 mx-8'>
-                <div className='font-clash-semibold  text-2xl  underline underline-offset-8  py-4 '>
-                    {items.Name}
-                </div>
-                <div className='font-clash-medium pl-4 font-thin text-lg'>
-                    <div className='py-1'>
-                        {items.Type}
-                    </div>
-                    <div className='py-1'>
-                        {items["Street Address"] + " " + items.Location}
-                    </div>
-                    <div className='py-1'>
-                        {"Reviews :" + items.Reviews}
-                    </div>
-                    <div className='py-1'>
-                        {"Price Range :  " + items.Price_Range}
-                    </div>
+            <div className='container my-8 mx-8 bg-orange-50 rounded-md px-8 py-4'>
+                <div className='flex flex-row space-x-10 pt-4 '>
 
-
-                    <div className='flex flex-row space-x-3 pl-2'>
-                        <div className='bg-tangerine rounded-lg text-xl w-max px-4 mt-4 py-1 text-white'>
-                            Menu
+                    <div className='basis-1/2'>
+                        <div className='font-clash-semibold  text-4xl'>
+                            {items.Name}
                         </div>
-                        <div className='bg-tangerine rounded-lg text-xl w-max px-4 mt-4 py-1 text-white'>
-                            TripAdvisor
-                        </div>
-                        <div className='bg-tangerine rounded-lg text-xl w-max px-4 mt-4 py-1 text-white'>
-                            Google Maps
+                        <div className='font-clash-medium pl-4 font-thin text-xl pt-4'>
+                            <div className='py-1'>
+                                {items.Type}
+                            </div>
+                            <div className='py-1'>
+                                {items["Street Address"] + " " + items.Location}
+                            </div>
+                            <div className='py-1'>
+                                {"Reviews :" + items.Reviews}
+                            </div>
+                            <div className='py-1'>
+                                {"Price Range :  " + items.Price_Range}
+                            </div>
                         </div>
                     </div>
 
-                    <div className='flex flex-col space-x-3 pt-4 pl-2'>
-                        <div className='text-xl font-clash-semibold text-slate-600 my-2'>Comments</div>
+                    <div className='basis-1/2 flex flex-row justify-center pr-8 pt-6 relative'>
 
-                        <div>{items.Comments}</div>
+                        <img className="w-40 z-20" src={`${svgPath}Dimsum.svg`}></img>
+
+
                     </div>
+
                 </div>
 
+                <div className='flex flex-col space-x-3 pt-4 pl-2'>
+                    <div className='text-xl font-clash-semibold text-slate-600 my-2 z-20'>Comments</div>
+                    <div className='text-lg font-clash-medium text-stone-700'>{items.Comments}</div>
+                </div>
+
+                <div className='flex flex-row space-x-3 pl-2'>
+                    <div className='bg-tangerine rounded-lg text-xl w-max px-4 mt-4 py-1 text-white'>
+                        Menu
+                    </div>
+                    <div className='bg-tangerine rounded-lg text-xl w-max px-4 mt-4 py-1 text-white'>
+                        TripAdvisor
+                    </div>
+                    <div className='bg-tangerine rounded-lg text-xl w-max px-4 mt-4 py-1 text-white'>
+                        Google Maps
+                    </div>
+                </div>
 
             </div>
 
