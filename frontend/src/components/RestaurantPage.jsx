3dc2421f0d077f61784de0f485ca4ca238677012
duranbe <benoit.durand.mail@gmail.com>
2022-12-02 19:48:27 +0100

    Implementing favorites feature using localStorage

--- frontend/src/components/RestaurantPage.jsx
+++ frontend/src/components/RestaurantPage.jsx
@@ -5,6 +5,74 @@ import axios from 'axios';
 
 
 const svgPath = process.env.PUBLIC_URL + '/svg/icons/';
+//const svg = process.env.PUBLIC_URL + '/svg/';
+
+function StarIcon({id}) {
+
+    const [isFavorite,setFavorite] = useState(false)
+
+
+    function handleOnClick() {
+       
+
+       if(isFavorite){
+        var favs = JSON.parse(localStorage.getItem("favorites"));
+        for( var i = 0; i < favs.length; i++){ 
+    
+            if ( favs[i] === id) { 
+        
+                favs.splice(i, 1); 
+            }
+        
+        }
+        console.log(favs)
+        localStorage.setItem("favorites",JSON.stringify(favs))
+        }else{
+    
+        console.log("Adding to favoerites")
+        var favorites = JSON.parse(localStorage.getItem("favorites"));
+        if(favorites){
+            favorites.push(id)
+        }else{
+            favorites = [id]
+        }
+        console.log(favorites)
+        localStorage.setItem("favorites",JSON.stringify(favorites))
+       }
+
+       setFavorite(!isFavorite)
+
+       
+    }
+
+
+    if(!isFavorite){
+        
+    return (
+        <div className='flex flex-col items-center justify-center p-4'>
+
+       
+        <svg onClick={() => { handleOnClick()}} className='h-8 w-8 cursor-pointer' xmlns="http://www.w3.org/2000/svg" viewBox="0 0 576 512"><path d="M528.1 171.5L382 150.2 316.7 17.8c-11.7-23.6-45.6-23.9-57.4 0L194 150.2 47.9 171.5c-26.2 3.8-36.7 36.1-17.7 54.6l105.7 103-25 145.5c-4.5 26.3 23.2 46 46.4 33.7L288 439.6l130.7 68.7c23.2 12.2 50.9-7.4 46.4-33.7l-25-145.5 105.7-103c19-18.5 8.5-50.8-17.7-54.6zM388.6 312.3l23.7 138.4L288 385.4l-124.3 65.3 23.7-138.4-100.6-98 139-20.2 62.2-126 62.2 126 139 20.2-100.6 98z" /></svg>
+        </div>
+    )
+
+    }else{
+
+        return(
+            <div className='flex flex-col items-center justify-center p-4'>
+            <svg onClick={() => { handleOnClick()}} className='h-8 w-8 cursor-pointer' xmlns="http://www.w3.org/2000/svg" viewBox="0 0 576 512"><path d="M259.3 17.8L194 150.2 47.9 171.5c-26.2 3.8-36.7 36.1-17.7 54.6l105.7 103-25 145.5c-4.5 26.3 23.2 46 46.4 33.7L288 439.6l130.7 68.7c23.2 12.2 50.9-7.4 46.4-33.7l-25-145.5 105.7-103c19-18.5 8.5-50.8-17.7-54.6L382 150.2 316.7 17.8c-11.7-23.6-45.6-23.9-57.4 0z" /></svg>
+            </div>
+        )
+
+    }
+
+
+
+
+
+}
+
+
 
 export default function RestaurantPage() {
     const { id } = useParams();
@@ -23,7 +91,7 @@ export default function RestaurantPage() {
 
             console.log(response.data)
             setItems(response.data.restaurantData)
-            if(response.data.restaurantData.icon.length>0){
+            if (response.data.restaurantData.icon.length > 0) {
                 setIcon(true);
             }
 
@@ -38,13 +106,15 @@ export default function RestaurantPage() {
         return (<div>Loading...</div>)
     } else {
         return (
-            <div className='container my-8 mx-8 bg-orange-50 rounded-md px-8 py-4'>
+            <div className='container my-8 mx-auto bg-orange-50 rounded-md px-8 py-4'>
                 <div className='flex flex-row space-x-10 pt-4 '>
 
                     <div className='basis-1/2'>
-                        <div className='font-clash-semibold  text-4xl underline underline-offset-4 decoration-tangerine decoration-4'>
-                            {items.Name}
+                        <div className='font-clash-semibold  text-4xl underline underline-offset-4 items-stretch decoration-tangerine decoration-4 flex flex-row'>
+                            <div>{items.Name}</div>
+                            <StarIcon id={id}/>
                         </div>
+                        
                         <div className='font-clash-medium pl-4 font-thin text-xl pt-4'>
                             <div className='py-1'>
                                 {items.Type}
@@ -70,7 +140,7 @@ export default function RestaurantPage() {
 
                     <div className='basis-1/2 flex flex-row justify-center pr-8 pt-6'>
 
-                            { hasIcon &&  <Icon iconArray={items.icon}/>}
+                        {hasIcon && <Icon iconArray={items.icon} />}
 
 
                     </div>
@@ -108,7 +178,7 @@ function Icon({ iconArray }) {
     const iconPath = iconArray[0].filename
     return (
 
-        <div className='img_bg bg-tangerine-light rounded-full w-60 relative'>
+        <div className='img_bg bg-tangerine-light rounded-full w-60 h-60 relative'>
             <img alt="icon" className="w-40 absolute top-0 left-0 right-0 bottom-0 m-auto p-1" src={`${svgPath}${iconPath}`}></img>
         </div>
 
