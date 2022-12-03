cde520ba90500998dca7d5744d2f5432cad5f16d
duranbe <benoit.durand.mail@gmail.com>
2022-12-03 22:30:30 +0100

    Add data fetching for favs

--- frontend/src/components/Favorites.jsx
+++ frontend/src/components/Favorites.jsx
@@ -1,16 +1,51 @@
 import React from 'react';
-
-
-
+import axios from 'axios';
+import { useState } from 'react';
+import { useEffect } from 'react';
 
 export default function Favorites() {
 
     var favoritesRestaurant = JSON.parse(localStorage.getItem("favorites"));
 
-    return (
-        <div className='my-8 mx-auto px-8 py-4'>
-            {favoritesRestaurant.map(item => <h1 key={item}>{item}</h1>)}
-        </div>
+        return (
+            <div className='my-8 mx-auto px-8 py-4'>
+                {favoritesRestaurant.map(item => <FavRestaurantCard key={item} propsId={item}/>)}
+            </div>
 
-    )
+        )
+
+    
+
+
 }
+
+
+function FavRestaurantCard({propsId}) {
+    const [data, setData] = useState(null);
+  
+    useEffect(() => {
+      const fetchData = async () => {
+        console.log(propsId)
+        axios.get('http://localhost:8000/get_restaurant', {
+                params: {
+                    restaurantId: propsId
+                }
+            }).then(function (response) {
+
+                setData(response.data.restaurantData)
+                console.log(response.data.restaurantData)
+
+            }).catch(function (error) {
+                console.log(error);
+            })
+      };
+  
+      fetchData();
+    }, [propsId]);
+  
+    if (data) {
+      return (<div>{data.Name}</div>);
+    } else {
+      return (<div>Loading </div>);
+    }
+  }
--- frontend/src/components/Footer.jsx
+++ frontend/src/components/Footer.jsx
@@ -5,7 +5,7 @@ export default function Footer(){
 
     return (
 
-        <footer className='font-clash-extralight font-bold text-center my-12 flex flex-row justify-center bottom-0'>
+        <footer className='font-clash-extralight text-xl font-bold text-center my-12 flex flex-row justify-center bottom-0'>
           <div className='px-4 hover:underline underline-offset-4'>About us</div>
           <div className='px-4 hover:underline underline-offset-4'><a href='https://github.com/duranbe/restaurants-reviews'>Github</a></div>
           <div className='px-4 hover:underline underline-offset-4'>Dev.to</div>
