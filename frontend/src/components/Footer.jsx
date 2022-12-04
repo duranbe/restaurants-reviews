ea3f0d86de930d114d6ebeb9925210017042d2ea
duranbe <benoit.durand.mail@gmail.com>
2022-12-04 14:08:08 +0100

    Adding not found text

--- frontend/src/components/Footer.jsx
+++ frontend/src/components/Footer.jsx
@@ -4,11 +4,14 @@ import React from 'react';
 export default function Footer(){
 
     return (
-
-        <footer className='font-clash-extralight text-xl font-bold text-center my-12 flex flex-row justify-center bottom-0'>
+        <footer className='bottom-0 w-full'>
+          <div className='font-clash-extralight text-xl font-bold text-center mt-10 mb-2 flex flex-row justify-center'>
           <div className='px-4 hover:underline underline-offset-4'>About us</div>
           <div className='px-4 hover:underline underline-offset-4'><a href='https://github.com/duranbe/restaurants-reviews'>Github</a></div>
           <div className='px-4 hover:underline underline-offset-4'>Dev.to</div>
+
+          </div>
+          
         </footer>
         
        
--- frontend/src/components/SearchResult.jsx
+++ frontend/src/components/SearchResult.jsx
@@ -1,18 +1,33 @@
 import RestaurantCard from "./RestaurantCard"
 
 export default function SearchResult({ data }) {
+    
 
-    return (
+    if(data.data.length===0){
+        return(
 
-        <div className='flex flex-col items-center'>
-            <div className='container flex flex-col justify-center relative max-w-4xl'>
-                <div className='absolute blob bg-tangerine z-10  top-40 left-10  h-[15rem] w-[15rem] m-0 p-0'></div>
-                <div className='absolute blob bg-yellow-light rotate-90 z-10  top-96 left-2/3  h-[20rem] w-[20rem] m-0 p-0'></div>
-                <div className=' text-black mx-8 pl-12 z-20'>
-                    {data.data.filter((val) => val.normalizedScore > 0.5).map((val) => (<RestaurantCard key={val.id} restaurantData={val} />))}
-                </div>
-
+            <div className='flex flex-col items-center my-12'>
+                <div className="font-clash-medium text-xl text-black">Nothing found :(</div>
             </div>
-        </div>)
+            
+            )
+    }else{
+
+        return (
+
+            <div className='flex flex-col items-center'>
+                <div className='container flex flex-col justify-center relative max-w-4xl'>
+                    <div className='absolute blob bg-tangerine z-10  top-40 left-10  h-[15rem] w-[15rem] m-0 p-0'></div>
+                    <div className='absolute blob bg-yellow-light rotate-90 z-10  top-96 left-2/3  h-[20rem] w-[20rem] m-0 p-0'></div>
+                    <div className=' text-black mx-8 pl-12 z-20'>
+                        {data.data.filter((val) => val.normalizedScore > 0.5).map((val) => (<RestaurantCard key={val.id} restaurantData={val} />))}
+                    </div>
+    
+                </div>
+            </div>
+        )
+
+    }
+    
 
 }
