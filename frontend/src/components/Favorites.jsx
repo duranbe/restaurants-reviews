02782b0686dcca1a29dbd940a76171644e1ed97d
duranbe <benoit.durand.mail@gmail.com>
2022-12-04 14:20:18 +0100

    Add icon to fav page

--- frontend/src/components/Favorites.jsx
+++ frontend/src/components/Favorites.jsx
@@ -5,53 +5,59 @@ import { useEffect } from 'react';
 
 export default function Favorites() {
 
-    var favoritesRestaurant = JSON.parse(localStorage.getItem("favorites"));
+  var favoritesRestaurant = JSON.parse(localStorage.getItem("favorites"));
 
-        return (
-            <div className='my-8 mx-auto px-8 py-4'>
-                {favoritesRestaurant.map(item => <FavRestaurantCard key={item} propsId={item}/>)}
-            </div>
+  return (
+    <div className='flex flex-row'>
 
-        )
 
-    
+      <div className='my-8 px-8 py-4 basis-1/2'>
+        {favoritesRestaurant.map(item => <FavRestaurantCard key={item} propsId={item} />)}
+      </div>
+      <div className='px-8 basis-1/3' >
+         <img classname="" src="./svg/DrawKit-cooking-kitchen-food-vector-illustrations-15.svg" alt="cooking kitchen shelf"></img>
+      </div>
+
+    </div>
+
+  )
 
 
 }
 
 
-function FavRestaurantCard({propsId}) {
-    const [data, setData] = useState(null);
-  
-    useEffect(() => {
-      const fetchData = async () => {
-        console.log(propsId)
-        axios.get('http://localhost:8000/get_restaurant', {
-                params: {
-                    restaurantId: propsId
-                }
-            }).then(function (response) {
+function FavRestaurantCard({ propsId }) {
+  const [data, setData] = useState(null);
 
-                setData(response.data.restaurantData)
-                
-            }).catch(function (error) {
-                
-            })
-      };
-  
-      fetchData();
-    }, [propsId]);
-  
-    if (data) {
-      return (
-        <div>
-             <div className='font-clash-semibold text-3xl py-2'>
-                <a href={`http://localhost:3000/ri/${propsId}`}>{data.Name}</a>
-            </div>
+  useEffect(() => {
+    const fetchData = async () => {
+      console.log(propsId)
+      axios.get('http://localhost:8000/get_restaurant', {
+        params: {
+          restaurantId: propsId
+        }
+      }).then(function (response) {
+
+        setData(response.data.restaurantData)
+
+      }).catch(function (error) {
+
+      })
+    };
+
+    fetchData();
+  }, [propsId]);
+
+  if (data) {
+    return (
+      <div>
+        <div className='font-clash-semibold text-3xl py-2 hover:underline underline-offset-2'>
+          <a href={`http://localhost:3000/ri/${propsId}`}>{data.Name}</a>
         </div>
-     
-      );
-    } else {
-      return (<div>Loading </div>);
-    }
+      </div>
+
+    );
+  } else {
+    return (<div>Loading </div>);
   }
+}
